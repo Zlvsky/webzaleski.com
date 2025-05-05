45c66ae0b9d0c1f6c59c8760fe97611e9bd04104
Zlvsky <zlvsky@icloud.com>
2025-05-06 00:54:30 +0200

    feat: added i18n

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import { NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
+ 
+
+const nextConfig = {
+  webpack(config: NextConfig) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    })
+
+    return config
+  }
+}
+
+const withNextIntl = createNextIntlPlugin()
+export default withNextIntl(nextConfig)
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,8 +1,12 @@
 import { ThemeProvider } from '@/components/providers/ThemeProvider'
+import { routing } from '@/i18n/routing'
 import { Analytics } from '@vercel/analytics/react'
 import type { Metadata } from 'next'
+import { hasLocale, NextIntlClientProvider } from 'next-intl'
 import { Inter } from 'next/font/google'
-import './globals.css'
+
+import { notFound } from 'next/navigation'
+import '../globals.css'
 
 const inter = Inter({ subsets: ['latin'], display: 'swap' })
 
@@ -12,11 +16,17 @@ export const metadata: Metadata = {
     'Web & Frontend developer. Specialising in development of web applications, MVPs and websites. JavaScript, React.js, Next.js.'
 }
 
-export default function RootLayout({
-  children
+export default async function RootLayout({
+  children,
+  params
 }: Readonly<{
   children: React.ReactNode
+  params: Promise<{ locale: string }>
 }>) {
+  const { locale } = await params
+  if (!hasLocale(routing.locales, locale)) {
+    notFound()
+  }
   return (
     <html lang="en" suppressHydrationWarning>
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon.png" />
@@ -24,9 +34,11 @@ export default function RootLayout({
       <link rel="icon" type="image/png" href="/favicon-16x16.png" />
       <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
-        </ThemeProvider>
+        <NextIntlClientProvider>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            {children}
+          </ThemeProvider>
+        </NextIntlClientProvider>
         <Analytics />
       </body>
     </html>
